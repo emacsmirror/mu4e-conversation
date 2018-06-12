be594005547c04fdd8ad51c8a8b7b8f9a6bfaf43
Pierre Neidhardt <ambrevar@gmail.com>
2018-06-12 15:56:48 +0200

    Update commentary with usage instructions

--- mu4e-conversation.el
+++ mu4e-conversation.el
@@ -27,6 +27,15 @@
 ;;; Commentary:
 ;; In this file we define mu4e-conversation-mode (+ helper functions), which is
 ;; used for viewing all e-mail messages of a thread in a single buffer.
+;;
+;; From the headers view, run the command `mu4e-conversation'.
+;;
+;; To fully replace `mu4e-view' with `mu4e-conversation' from any other command
+;; (e.g. `mu4e-headers-next', `helm-mu'), call
+;;
+;;   (setq mu4e-view-func 'mu4e-conversation)
+
+;;; Code:
 
 ;; TODO: Overrides are not commended.  Use unwind-protect to set handlers?  I don't think it would work.
 ;; TODO: Only mark visible messages as read.
@@ -34,7 +43,6 @@
 ;; TODO: Detect subject changes.
 ;; TODO: Check out mu4e gnus view.
 
-;;; Code:
 (require 'mu4e)
 (require 'rx)
 (require 'outline)
